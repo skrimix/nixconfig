86acc806d8ab288756814b4042daabaa54ec207a
skrimix <28156185+skrimix@users.noreply.github.com>
2024-03-19 02:32:48 +0500

    generation 259

--- rebuild.sh
+++ rebuild.sh
@@ -2,19 +2,23 @@
 set -e
 pushd "$(dirname "$0")" > /dev/null
 
+_echo() {
+    echo -e "\e[1;32m$1\e[0m"
+}
+
 # check for changes and --no-git flag
 if [ -z "$(git status --porcelain)" ] || [ "$1" == "--no-git" ]; then
-    echo "No changes to commit or no-git flag used."
-    echo "Rebuilding NixOS..."
+    _echo "No changes to commit or no-git flag used."
+    _echo "Rebuilding NixOS..."
     sudo nixos-rebuild switch --flake path:/home/skrimix/.nix
     exit 0
 fi
 
 
 git diff --color -U0
-echo "Rebuilding NixOS..."
+_echo "Rebuilding NixOS..."
 sudo nixos-rebuild switch --flake path:/home/skrimix/.nix
 gen=$(nixos-rebuild list-generations --flake path:/home/skrimix/.nix | grep current | awk '{print $1}')
 git add -A
 git commit -am "generation $gen"
-git push > /dev/null && echo "Pushed to git"
+git push > /dev/null && _echo "Pushed to git"
