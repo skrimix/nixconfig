bb4f70ff95139565f20a208d2c138d1fbef42c73
skrimix <28156185+skrimix@users.noreply.github.com>
2024-03-19 02:41:53 +0500

    generation 262

--- rebuild.sh
+++ rebuild.sh
@@ -14,14 +14,14 @@ _echo_fail() {
 if [ -z "$(git status --porcelain)" ] || [ "$1" == "--no-git" ]; then
     _echo "No changes to commit or no-git flag used."
     _echo "Rebuilding NixOS..."
-    sudo nixos-rebuild switch --flake path:/home/skrimix/.nix && _echo "Rebuild successful" || _echo_fail "Rebuild failed" && exit 1
+    (sudo nixos-rebuild switch --flake path:/home/skrimix/.nix && _echo "Rebuild successful") || (_echo_fail "Rebuild failed" && exit 1)
     exit 0
 fi
 
 
 git diff --color -U0
 _echo "Rebuilding NixOS..."
-sudo nixos-rebuild switch --flake path:/home/skrimix/.nix && _echo "Rebuild successful, committing to git" || _echo_fail "Rebuild failed" && exit 1
+(sudo nixos-rebuild switch --flake path:/home/skrimix/.nix && _echo "Rebuild successful, committing to git") || (_echo_fail "Rebuild failed" && exit 1)
 gen=$(nixos-rebuild list-generations --flake path:/home/skrimix/.nix | grep current | awk '{print $1}')
 git add -A
 git commit -am "generation $gen"
