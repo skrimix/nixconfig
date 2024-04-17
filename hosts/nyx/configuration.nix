3e3481a00c0956b85c2fb0ab4335ca427590ce5b
skrimix <28156185+skrimix@users.noreply.github.com>
2024-04-17 21:59:28 +0500

    generation 412

--- hosts/nyx/configuration.nix
+++ hosts/nyx/configuration.nix
@@ -142,11 +142,6 @@
       DefaultTimeoutStopSec=20s
     '';
 
-    additionalUpstreamSystemUnits = [
-      "soft-reboot.target"
-      "systemd-soft-reboot.service"
-    ];
-
     # disable service
     services.ollama.wantedBy = lib.mkForce [ ];
 
