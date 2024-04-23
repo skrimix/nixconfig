9af883ea172e172f7ed66ff4da821cdab3942477
skrimix <28156185+skrimix@users.noreply.github.com>
2024-04-23 13:11:58 +0500

    generation 431

--- flake.nix
+++ flake.nix
@@ -4,14 +4,18 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nix-alien.url = "github:thiagokokada/nix-alien";
+    nixd.url = "github:nix-community/nixd";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, nixd, ... }@inputs: {
     nixosConfigurations = {
       nyx = nixpkgs.lib.nixosSystem rec {
         system = "x86_64-linux";
         specialArgs = { inherit self system inputs; };
         modules = [
+          {
+            nixpkgs.overlays = [ nixd.overlays.default ];
+          }
           ./packages
           ./modules/nix-alien.nix
           ./modules/kb-switch.nix
